1dd8fb442f0d8c2fe9063a52330091ca68703865
renan olovics <renanolovics@gmail.com>
2025-06-15 16:15:16 -0300

    feat: console.log work with us

--- src/providers/AppProvider.tsx
+++ src/providers/AppProvider.tsx
@@ -1,7 +1,20 @@
-import { PropsWithChildren } from 'react'
+import { PropsWithChildren, useEffect } from 'react'
 
 import { QueryProvider } from './QueryProvider'
 
-export const AppProvider = ({ children }: PropsWithChildren) => (
-  <QueryProvider>{children}</QueryProvider>
-)
+export const AppProvider = ({ children }: PropsWithChildren) => {
+  useEffect(() => {
+    console.log(`
+  _____  _____  _____  ______
+  |  __ |_   _|/ ____||___  /
+  | |__) | | | | |  __    / /
+  |  ___/  | | | | |_ |  / /
+  | |     _| |_| |__| | / /__
+  |_|    |_____|_____|/_____|
+
+  Trabalhe conosco, veja as vagas em:
+  https://jobs.quickin.io/pigz/jobs
+    `)
+  }, [])
+  return <QueryProvider>{children}</QueryProvider>
+}
